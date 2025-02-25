253cf9d8b6d5ca5f6bb1e74b50121d8302901ae7
Tibo Ulens <ulens.tibo@gmail.com>
2025-02-25 15:50:08 +0100

    fix: add defaults for db envvars

--- app/database.py
+++ app/database.py
@@ -3,11 +3,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-_db_user = os.environ.get("DATABASE_USER")
-_db_pwd = os.environ.get("DATABASE_PASSWORD")
-_db_host = os.environ.get("DATABASE_HOST")
-_db_port = os.environ.get("DATABASE_PORT")
-_db_name = os.environ.get("DATABASE_NAME")
+_db_user = os.getenv("DATABASE_USER", "blokmap")
+_db_pwd = os.getenv("DATABASE_PASSWORD", "appel")
+_db_host = os.getenv("DATABASE_HOST", "127.0.0.1")
+_db_port = os.getenv("DATABASE_PORT", "5432")
+_db_name = os.getenv("DATABASE_NAME", "blokmap")
 
 url = f"postgresql+psycopg://{_db_user}:{_db_pwd}@{_db_host}:{_db_port}/{_db_name}"
 
