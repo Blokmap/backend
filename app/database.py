3ddf2786f098f0b4f95b312e2ff0ddc0155e8f85
Tibo Ulens <ulens.tibo@gmail.com>
2025-02-25 15:46:15 +0100

    feat: read db port from env

--- app/database.py
+++ app/database.py
@@ -6,9 +6,10 @@ from sqlalchemy.orm import sessionmaker
 _db_user = os.environ.get("DATABASE_USER")
 _db_pwd = os.environ.get("DATABASE_PASSWORD")
 _db_host = os.environ.get("DATABASE_HOST")
+_db_port = os.environ.get("DATABASE_PORT")
 _db_name = os.environ.get("DATABASE_NAME")
 
-url = f"postgresql+psycopg://{_db_user}:{_db_pwd}@{_db_host}:5432/{_db_name}"
+url = f"postgresql+psycopg://{_db_user}:{_db_pwd}@{_db_host}:{_db_port}/{_db_name}"
 
 engine = create_engine(url)
 
