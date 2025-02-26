66702e3814947e905e1ca7173863b2358d3a4068
Tibo Ulens <ulens.tibo@gmail.com>
2025-02-26 20:30:24 +0100

    feat: implement basic user auth

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
-from typing import Annotated
+from fastapi import FastAPI
 
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-
-from .model import User
+from .routers import auth, user
 
 from .routers import auth, user
 
