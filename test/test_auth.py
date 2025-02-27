495ab473e276b0dff1eef5620453661103d31303
Tibo Ulens <ulens.tibo@gmail.com>
2025-02-27 17:04:47 +0100

    feat: add login test

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,6 +1,8 @@
+import jwt
 from fastapi.testclient import TestClient
 
 from app.main import app
+from app.constants import JWT_ALGORITHM, JWT_SECRET_KEY
 
 
 client = TestClient(app)
@@ -18,3 +20,22 @@ def test_signup():
     assert data["username"] == "bob"
     assert data["email"] == "bob@example.com"
     assert data["hashed_password"] is not None
+
+
+def test_login():
+    response = client.post(
+        "/auth/login",
+        data={"username": "bob", "password": "appel"},
+    )
+    data = response.json()
+
+    assert response.status_code == 201
+    assert data["token_type"] == "bearer"
+    assert data["access_token"] is not None
+
+    payload = jwt.decode(
+        data["access_token"], JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM]
+    )
+    id = payload.get("sub")
+
+    assert id is not None
