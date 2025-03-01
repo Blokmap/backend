b812a77ce1faaeb84136acaba47a6a14e763505e
Tibo Ulens <ulens.tibo@gmail.com>
2025-03-01 23:27:02 +0100

    refactor: cleanse everything with holy fire

--- Makefile
+++
@@ -1,7 +0,0 @@
-.PHONY: shell migrate
-
-shell:
-	docker exec -it -w /blokmap-backend blokmap-dev-backend /bin/sh
-
-migrate:
-	docker exec -w /blokmap-backend blokmap-dev-backend alembic upgrade head
--- app/dependencies/database.py
+++
@@ -1,8 +0,0 @@
-from typing import Annotated
-
-from fastapi import Depends
-from sqlmodel import Session
-
-from app.database import get_session
-
-DbSessionDep = Annotated[Session, Depends(get_session)]
