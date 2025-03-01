a2de996b417a4c1ba851248ae7e575f52fe01ab6
Tibo Ulens <ulens.tibo@gmail.com>
2025-03-01 17:12:22 +0100

    test: use single db instance with rollbacks

--- test/fixtures/session.py
+++ test/fixtures/session.py
@@ -1,23 +1,66 @@
+# See https://stackoverflow.com/a/67348153/14967861
+
 import pytest
+import sqlalchemy as sa
 from alembic import command
 from alembic.config import Config
 from sqlmodel import Session, create_engine
 from sqlmodel.pool import StaticPool
 
 
+# Create an sqlite connection which doesn't automatically store transactions
+engine = create_engine(
+    "sqlite://",
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+
+
+# Run migrations
+alembic_cfg = Config("alembic.ini")
+alembic_cfg.set_main_option("is_testing", "true")
+with engine.begin() as connection:
+    alembic_cfg.attributes["connection"] = connection
+    command.upgrade(alembic_cfg, "head")
+
+
+# These two event listeners are only needed for sqlite for proper
+# SAVEPOINT / nested transaction support. Other databases like postgres
+# don't need them.
+# From: https://docs.sqlalchemy.org/en/14/dialects/sqlite.html#serializable-isolation-savepoints-transactional-ddl
+@sa.event.listens_for(engine, "connect")
+def do_connect(dbapi_connection, connection_record):
+    # disable pysqlite's emitting of the BEGIN statement entirely.
+    # also stops it from emitting COMMIT before any DDL.
+    dbapi_connection.isolation_level = None
+
+
+@sa.event.listens_for(engine, "begin")
+def do_begin(conn):
+    # emit our own BEGIN
+    conn.exec_driver_sql("BEGIN")
+
+
 @pytest.fixture(name="session")
 def session_fixture():
-    engine = create_engine(
-        "sqlite://",
-        connect_args={"check_same_thread": False},
-        poolclass=StaticPool,
-    )
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = Session(autocommit=False, autoflush=False, bind=connection)
 
-    alembic_cfg = Config("alembic.ini")
-    alembic_cfg.set_main_option("is_testing", "true")
-    with engine.begin() as connection:
-        alembic_cfg.attributes["connection"] = connection
-        command.upgrade(alembic_cfg, "head")
+    # Begin a nested transaction (using SAVEPOINT).
+    nested = connection.begin_nested()
 
-    with Session(engine) as session:
-        yield session
+    # If the application code calls session.commit, it will end the nested
+    # transaction. Need to start a new one when that happens.
+    @sa.event.listens_for(session, "after_transaction_end")
+    def end_savepoint(session, transaction):
+        nonlocal nested
+        if not nested.is_active:
+            nested = connection.begin_nested()
+
+    yield session
+
+    # Rollback the overall transaction, restoring the state before the test ran.
+    session.close()
+    transaction.rollback()
+    connection.close()
