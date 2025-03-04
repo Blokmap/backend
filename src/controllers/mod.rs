dacd13c1bb3c3dd817f5629f94c7ac01895fa8e1
Tibo Ulens <ulens.tibo@gmail.com>
2025-03-04 15:59:52 +0100

    refactor!: simplify healthcheck

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -1,10 +1,8 @@
 //! Defines controller functions that correspond to individual routes
 
-use axum::Json;
 use axum::extract::State;
-use axum::http::StatusCode;
+use axum::response::NoContent;
 use diesel::{RunQueryDsl, sql_query};
-use serde_json::{Value, json};
 
 use crate::DbPool;
 use crate::error::Error;
@@ -15,10 +13,10 @@ pub mod translation;
 /// Check if the database connection and webserver are functional
 pub(crate) async fn healthcheck(
 	State(pool): State<DbPool>,
-) -> Result<(StatusCode, Json<Value>), Error> {
+) -> Result<NoContent, Error> {
 	let conn = pool.get().await?;
 
 	conn.interact(|conn| sql_query("SELECT 1").execute(conn)).await??;
 
-	Ok((StatusCode::OK, Json(json!({ "status": "ok" }))))
+	Ok(NoContent)
 }
