ae267c13f4cb46369173c9607dcec3fbd989c20e
Tibo Ulens <ulens.tibo@gmail.com>
2025-03-08 01:23:25 +0100

    test: add duplicate email test

--- tests/auth.rs
+++ tests/auth.rs
@@ -9,7 +9,7 @@ mod common;
 
 use blokmap::models::Profile;
 use common::get_test_app;
-use common::wrappers::{expect_mail, expect_no_mail};
+use common::wrappers::{expect_mail, expect_mail_to, expect_no_mail};
 
 #[tokio::test(flavor = "multi_thread")]
 async fn register() {
@@ -185,6 +185,40 @@ async fn register_invalid_email() {
 
 	assert_eq!(response.status_code(), StatusCode::UNPROCESSABLE_ENTITY);
 	assert_eq!(body, "invalid email".to_string());
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn register_duplicate_email() {
+	let (_guard, mailbox, test_server) = get_test_app(false).await;
+
+	expect_mail_to(mailbox.clone(), vec!["bob@example.com"], async || {
+		test_server
+			.post("/auth/register")
+			.json(&RegisterRequest {
+				username: "bob".to_string(),
+				password: "bobdebouwer1234!".to_string(),
+				email:    "bob@example.com".to_string(),
+			})
+			.await
+	})
+	.await;
+
+	let response = expect_no_mail(mailbox, async || {
+		test_server
+			.post("/auth/register")
+			.json(&RegisterRequest {
+				username: "bob2".to_string(),
+				password: "bobdebouwer1234!".to_string(),
+				email:    "bob@example.com".to_string(),
+			})
+			.await
+	})
+	.await;
+
+	let body = response.text();
+
+	assert_eq!(response.status_code(), StatusCode::CONFLICT);
+	assert_eq!(body, "pending is already in use".to_string());
 }
 
 #[tokio::test(flavor = "multi_thread")]
