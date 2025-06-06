b4882aae6f8911131f32811e93c8c0672353abaa
Tibo Ulens <ulens.tibo@gmail.com>
2025-06-06 11:37:15 +0200

    feat: bit more safety in migrations

--- migrations/00000000000000_diesel_initial_setup/up.sql
+++ migrations/00000000000000_diesel_initial_setup/up.sql
@@ -35,4 +35,4 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE COLLATION case_insensitive (provider = icu, locale = 'und-u-ks-level2', deterministic = false);
+CREATE COLLATION IF NOT EXISTS case_insensitive (provider = icu, locale = 'und-u-ks-level2', deterministic = false);
